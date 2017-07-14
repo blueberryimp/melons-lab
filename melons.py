5f9950d8e9ec7c56d7919f70922baca89ebd00c2
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-13 17:35:50 -0700

    oomelon wip

--- melons.py
+++ melons.py
@@ -1,4 +1,5 @@
 """Classes for melon orders."""
+from random import randint
 
 
 class AbstractMelonOrder(object):
@@ -13,11 +14,21 @@ class AbstractMelonOrder(object):
         self.order_type = order_type
         self.tax = tax
 
+    def get_base_price(self):
+
+        self.base_price = randint(5, 9)
+        return self.base_price
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()
+        if self.species == "christmas melons":
+            base_price *= 1.5
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
 
         return total
 
@@ -31,14 +42,16 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty, order_type, tax):
-        super(DomesticMelonOrder, self).__init__(species, qty, order_type='domestic', tax=0.08)
+        super(DomesticMelonOrder, self).__init__(species, qty, 'domestic', 0.08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, order_type, tax, country_code):
-        super(DomesticMelonOrder, self).__init__(species, qty, order_type='international', tax=0.17)
+        """ A melon order outside the USA. """
+
+        super(InternationalMelonOrder, self).__init__(species, qty, 'international', 0.17)
 
         country_code = country_code
 
@@ -46,3 +59,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order purchased by the US Government."""
+
+    def __init__(self, species, qty, order_type, tax):
+        super(GovernmentMelonOrder, self).__init__(species, qty, 'government', 0)
+
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
+        return self.passed_inspection
